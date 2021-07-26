dfbd7d041558475b94a4faddaf9a9d205a14e632
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-07-26 12:59:42 -0600

    adds bounties app to bounties tab, modifies Footer.js to include attributions

--- src/views/Bounties/Bounties.js
+++ src/views/Bounties/Bounties.js
@@ -1,28 +1,170 @@
-import React from 'react';
-import './Bounties.scss';
-import { Button } from "antd";
-
-import Icons from '../../Icons';
-
+import React, { useState, useEffect } from "react";
+import "./Bounties.scss";
+import Icons from "../../Icons";
+//Ant D imports
+import { Button, Table } from "antd";
+import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
+//Component Imports
+import Header from "./Header.js";
+import ClaimModal from "./ClaimModal.js";
 
 function Bounties() {
-  window.scrollTo(0, 0);
+  let initialJobForm = {
+    jobTitle: "",
+    jobType: "",
+  };
+
+  const [rawData, setRawData] = useState();
+  const [bountiesData, setBountiesData] = useState();
+  const [jobForm, setJobForm] = useState(initialJobForm);
+
+  //useEffect to populate the table with data from the Sheety API
+  useEffect(() => {
+    const bountiesUrl =
+      "https://api.sheety.co/ed9240fc3b351479d6da738838e4133d/tellorBountiesProgram/bounties";
+    fetch(bountiesUrl)
+      .then((response) => response.json())
+      .then((result) => {
+        setRawData(result.bounties);
+        dataHelper(result.bounties);
+      });
+  }, []);
+
+  //DataHelper function to get Sheety API data into proper form for the AntD Table
+  const dataHelper = (unformattedData) => {
+    let dataArray = [];
+    let randomBountyObj = {
+      key: 100,
+      title: "Suggest A Bounty",
+      jobType: "Various",
+      tributes: "Various",
+      available: "Yes",
+      description:
+        "Have a suggestion? After you claim this bounty, let us know about your idea on how to expand Tellor in the comments section!",
+    };
+    unformattedData.forEach((data) => {
+      let obj = {
+        key: data.id,
+        title: data.bountiesTitle ? data.bountiesTitle : null,
+        jobType: data.bountiesType ? data.bountiesType : null,
+        tributes: data.tributes ? data.tributes : null,
+        available: data.available ? data.available : null,
+        description: data.description ? data.description : null,
+        skills: data.skills ? data.skills : null,
+        notes: data.notes ? data.notes : null,
+      };
+      if (obj.title && obj.jobType) {
+        dataArray.push(obj);
+      }
+    });
+    dataArray.push(randomBountyObj);
+    setBountiesData(dataArray);
+  };
+
+  //AntD Column Logic
+  const columns = [
+    {
+      title: "Job Title",
+      dataIndex: "title",
+    },
+    {
+      title: "Job Type",
+      dataIndex: "jobType",
+    },
+    {
+      title: "Bounty",
+      dataIndex: "tributes",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.tributes - b.tributes,
+    },
+    {
+      title: "Available",
+      dataIndex: "available",
+    },
+    {
+      title: "Action",
+      dataIndex: "",
+      key: "x",
+      render: () => (
+        <Button id="claimModalButton" onClick={openClaimModal}>
+          Claim
+        </Button>
+      ),
+    },
+  ];
+
+  //Claim Modal Function
+  const openClaimModal = () => {
+    const claimModal = document.getElementById("claimModal");
+    claimModal.style.display = "block";
+  };
+  //Window function to help close modals
+  window.onclick = (event) => {
+    const claimModal = document.getElementById("claimModal");
+    const modal = document.getElementById("Modal");
+    if (event.target === claimModal) {
+      claimModal.style.display = "none";
+    } else if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
   return (
     <div>
-      <section className="contentSection">
-        <div className="contentTxt">
-          <h2>Bounties tellor</h2>
-        </div>
-      </section>
-
-      <section className="ButtonSection">  
+      <div className="App__Container">
+        <Header rawData={rawData} />
+        <Table
+          pagination={false}
+          columns={columns}
+          onRow={(record) => {
+            return {
+              onClick: () => {
+                setJobForm({
+                  jobTitle: record.title,
+                  jobType: record.jobType,
+                });
+              },
+            };
+          }}
+          expandable={{
+            expandedRowRender: (record) => (
+              <>
+                <p style={{ margin: 0 }}>
+                  Description: {record.description ? record.description : "N/A"}
+                </p>
+                <p style={{ margin: 0 }}>
+                  Necessary Skill(s): {record.skills ? record.skills : "N/A"}
+                </p>
+                {record.notes ? (
+                  <p style={{ margin: 0 }}>
+                    {" "}
+                    Notes:{" "}
+                    <a style={{ marginLeft: "5px" }} href={record.notes}>
+                      {record.notes}
+                    </a>
+                  </p>
+                ) : null}
+              </>
+            ),
+            expandIcon: ({ expanded, onExpand, record }) =>
+              expanded ? (
+                <MinusCircleOutlined onClick={(e) => onExpand(record, e)} />
+              ) : (
+                <PlusCircleOutlined onClick={(e) => onExpand(record, e)} />
+              ),
+            rowExpandable: (record) => record.builds !== "Not Expandable",
+          }}
+          dataSource={bountiesData}
+        />
+      </div>
+      <div id="claimModal" className="Claim__Modal">
+        <ClaimModal jobForm={jobForm} />
+      </div>
+      <section className="ButtonSection">
         <p className="smoltxt">Want to reach out?</p>
         <a href="https://discord.gg/n7drGjh" alt="link to Tellor on Discord">
-          <Button
-            shape="round"
-            size="large"
-            className="whitebtn"
-            ><Icons.Discord fill="#555555" />
+          <Button shape="round" size="large" className="whitebtn">
+            <Icons.Discord fill="#555555" />
             Talk to us on Discord
           </Button>
         </a>
---
+++ src/views/Bounties/Header.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+//Design imports
+import { ReactComponent as TellorBounties } from "./bountiesAssets/tellorbounties_renogare.svg";
+import { Button } from "antd";
+//Component imports
+import Welcome from "./Welcome.js";
+
+function Header({ rawData }) {
+  const [tellorBountiesAvailableData, setTellorBountiesAvailableData] =
+    useState();
+
+  //useEffect to call the Sheety API to render proper amount of available Tellor Bounties
+  useEffect(() => {
+    rawData &&
+      rawData.forEach((bounty) => {
+        if (bounty.bountiesTitle === "Tellor Bounties Available") {
+          setTellorBountiesAvailableData(bounty);
+        }
+      });
+
+    //Modal Pop-Up after 2 seconds
+    setTimeout(() => {
+      const modal = document.getElementById("Modal");
+      modal.classList.add("fadeIn");
+      modal.style.display = "block";
+    }, 2000);
+    setTimeout(() => {
+      const modal = document.getElementById("Modal");
+      modal.classList.remove("fadeIn");
+    }, 4000);
+  }, [rawData]);
+
+  //Modal Open/Close Functions
+  const openModal = () => {
+    const modal = document.getElementById("Modal");
+    modal.style.display = "block";
+  };
+  const closeModal = () => {
+    const modal = document.getElementById("Modal");
+    modal.style.display = "none";
+  };
+
+  return (
+    <div className="Header">
+      <TellorBounties className="Header__Logo" />
+      <div className="Available__Bounties">
+        <h2>Available Bounties</h2>
+        <Button id="Special__Button">
+          {tellorBountiesAvailableData && tellorBountiesAvailableData.tributes}
+        </Button>
+        <h4>
+          {tellorBountiesAvailableData &&
+            tellorBountiesAvailableData.description}
+        </h4>
+      </div>
+      <Button id="openModalButton" onClick={openModal}>
+        Ground Rules
+      </Button>
+      <div id="Modal" className="Modal">
+        <Welcome closeModal={closeModal} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
